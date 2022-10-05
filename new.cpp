bef62189c5709378cd9cf50b769e2139d272d47d
shakibul <shakibul.alam@bracits.com>
2022-10-06 00:08:23 +0600

    new cpp added

---
+++ new.cpp
@@ -0,0 +1,43 @@
+// Your First C++ Program
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int i = 23;
+
+int main()
+{
+    // int t;
+    // string s = "Hello World asdasdas!";
+    // cout << s.substr(0, 3) << endl;
+    // cin >> t;
+    // cout << endl << t;
+    // for (int i = 0; i < t; i++)
+    // {
+    //     cout<<"Sorry";
+    // }
+    // int a,b;
+    // cin >> a >> b;
+    // cout<< fmax(a, b);
+
+    // int i = i;
+    // cout << i;
+
+    // // string between spaces as input
+    // string name;
+    // cout << "enter your name: " << endl;
+    // getline(cin, name);
+    // cout << name;
+
+    // int nums[20] = {2, 4, 6, 7, 435, 4345, 3423, '2'};
+    // for (size_t i = 0; i < 20; i++)
+    // {
+    //     cout << nums[i] << ", ";
+    // }
+
+
+    
+
+    return 0;
+}
